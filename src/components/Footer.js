34879aa3e35f1234719a09eb487f90200310cf8e
mjstrz <strzelecki.monica7@gmail.com>
2022-05-31 16:25:52 -0400

    Footer social media icons & contact icons added (Font Awesome)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,34 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
+import styled from "styled-components";
+import {SocialMediaButtons, SocialButtonsContainer} from 'react-social-media-buttons';
 
 
 export default function Footer() {
     return(
         
             <div className="container">      
-                <div className="row row-content">
-                    <div className="col col-md-2">
-                    <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
-                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
-                    </div>
+                {/* <div className="row row-content"> */}
+                    {/* <div className="col col-md-2">
+
+                    </div> */}
 
                     <div className="contact col-lg-10 col-md-6 col-sm-2">
                         <div className='row row-content'>
                         <ul className='contact-footer'>
-                            <li>15 Lorem Ipsum Ln,Boston, MA</li>
-                            <li>(555).555.5555 </li>
-                            <li>fakeemail@fake.com</li>
+                            <i class="fa fa-map-marker"></i><li>15 Lorem Ipsum Ln,Boston, MA</li>
+                            <i class="fa fa-phone"></i><li>(555).555.5555 </li>
+                            <i class="fa fa-envelope"></i><li>fakeemail@fake.com</li>
                         </ul>
                         </div>
                     </div>
-                </div>
+                {/* </div> */}
+
+                <section className='social-media'>
+                    <h5>Social</h5>
+                    <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
+                    <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}   
+                </section>
             
                 <footer className='site-footer'>      
                     <h5>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>
