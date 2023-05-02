cb1b3b433f0a18d125e9cc423dc8c5028023af15
mjstrz <strzelecki.monica7@gmail.com>
2023-05-01 20:33:02 -0400

    cleaned up App.js file - keeping original BrowserRouter settings (not deploying from Github pages anymore)

--- src/App.js
+++ src/App.js
@@ -8,79 +8,7 @@ import React from 'react';
 import Main from './components/Main';
 import { BrowserRouter } from 'react-router-dom';
 
-// function Test() {
-//     return (
-//       <div>
-//           <h1>Hello, World</h1>
-//       </div>
-//     )
-// }
 
-// function Contact() {
-//     return (
-//       <div>
-//           <h1>Contact...please don't</h1>
-//       </div>
-//     )
-// }
-
-// function Chef() {
-//    return (
-//       <div>
-//          <h1>Bonjour, je m'appelle Romain</h1>
-//       </div>
-//    )
-// }
-
-
-// function App() {
-//   return (
-//     <div className="container">
-//     <Header />,
-//     <Navigation />,
-//     <Home />
-
-//     <Router>
-//          <div>
-//             <nav>
-//                <ul>
-//                   <li>
-//                      <Link to="/">Home</Link>
-//                   </li>
-//                   <li>
-//                      <Link to="/about">Our Story</Link>
-//                   </li>
-//                   <li>
-//                      <Link to="/contact">Contact</Link>
-//                   </li>
-//                   <li>
-//                      <Link to="/chef">Chef Romain</Link>
-//                   </li>
-//                </ul>
-//             </nav>
-
-//             <Switch>
-//                <Route path="/about">
-//                   <About />
-//                </Route>
-//                <Route path="/contact">
-//                   <Contact />
-//                </Route>
-//                <Route path="/chef">
-//                   <Chef />
-//                </Route>
-//                <Route path="/">
-//                   <Test />
-//                </Route>
-//             </Switch>
-//          </div>
-//       </Router>
-
-//     ,
-//     <Footer />,
-//     </div>
-//   );
-// }
 
 function App() {
    return (
